fc15f4b7bf96c2408772109b65e69d8351775028
Slava <kruchkovenko@gmail.com>
2024-04-07 03:39:51 +0500

    remove logs

--- src/main.py
+++ src/main.py
@@ -15,12 +15,10 @@ async def create_upload_file(file: UploadFile):
         filename = f"{file.filename}_{uuid.uuid4()}.{file.content_type.split('/')[1]}"
         project_dir = os.path.dirname(os.path.abspath(__file__))
         file_path = os.path.join(project_dir, filename)
-        print(file_path)
         with open(file_path, "wb") as f:
             f.write(bytes_io)
         points = get_point_by_image(file_path)
         os.remove(file_path)
-        print(len(points))
         return points
 
     except Exception as e:
